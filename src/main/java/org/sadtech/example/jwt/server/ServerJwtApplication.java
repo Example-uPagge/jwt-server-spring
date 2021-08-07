8afcbd2330706a6ef5062156370184720dec9ff9
Struchkov Mark <git@upagge.ru>
2021-08-07 18:11:18 +0300

    init commit

---
+++ src/main/java/org/sadtech/example/jwt/server/ServerJwtApplication.java
@@ -0,0 +1,13 @@
+package org.sadtech.example.jwt.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sadtech/example/jwt/server/ServerJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sadtech.example.jwt.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
